9cddb108d6ef1e013915827a2d783bec47c419d1
Berat Koçak <42033267+Reitnos@users.noreply.github.com>
2021-02-28 21:06:00 +0300

    28.02 created a basic todo list

--- App.tsx
+++ App.tsx
@@ -1,52 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React , {useState} from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from "./components/header";
+import TodoItem from "./components/todoItem";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
-   
+   },
+  content:
+  {
+    padding : 40,
   },
-  item:{
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24
+  list:{
+    marginTop: 20,
   }
-
-
 });
 
 export default function App() {
- const [people,setPeople] = useState([
-   { name: "shaun", key: "1"},
-   { name: "yoshi", key: "2"},
-   { name: "berat", key: "3"},
-   { name: "shaun2", key: "4"},
-   { name: "yoshi2", key: "5"},
-   { name: "berat2", key: "6"},
-   { name: "shaun3", key: "7"},
-   { name: "yoshi3", key: "8"},
-   { name: "berat3", key: "9"},
- ]);
+
+  const [todos,setTodos] = useState([
+    {text: "buy coffee", key: "1"},
+    {text: "create an app", key: "2"},
+    {text: "play on the switch", key: "3"},
+
+  ]);
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+   };
+
   return( 
-    // map maps on every item in the array. as parameter we do not have to use item. It is automatically set.
+  
     <View style= {styles.container} > 
-      <ScrollView>
-        {people.map((item) => {
-          return (
-              <View key = {item.key}>
-                <Text style = {styles.item}> {item.name} </Text>
-                </View>
-          )
-        })}
-        </ScrollView>
+      <Header/>
+      <View style = {styles.content}>
+        {/* to form */}
+        <View style = {styles.list}>
+            <FlatList 
+            data = {todos}
+            renderItem = {({item}) => (
+              <TodoItem item= {item} pressHandler = {pressHandler}/>
+            )}
+            />
+        </View>
+      </View>
     </View>
      
-    // here
+  
   );
 }
 
